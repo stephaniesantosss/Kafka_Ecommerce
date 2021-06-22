759eddccd849e5cdb3631f6ed922f37f1fc054df
Stephaniemaddison <Stephanieps2016@hotmail.com>
2021-06-22 15:26:36 -0300

    Projeto de kafka implementado com microsserviços finalizado com sucesso :)

---
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudeDetectorService.java
@@ -0,0 +1,36 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.HashMap;
+
+public class FraudeDetectorService {
+
+    public static void main(String[] args) {
+        var fraudService = new FraudeDetectorService();
+        try (var service = new KafkaService<>(FraudeDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudService::parse, Order.class, new HashMap<>())) {
+            service.run();
+        }
+
+    }
+
+    private void parse(ConsumerRecord<String, Order> record) {
+        System.out.println("____________________________________________");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            //ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Order processed");
+
+
+    }
+}
